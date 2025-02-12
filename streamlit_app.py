6777397a54e71917536605a926dfdab7781ee179
winniegu7 <winniegu7@gmail.com>
2025-02-12 16:39:42 -0500

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,8 +47,9 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen+' Nutrition Information ')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
-        sf_df=st.dataframe( data=smoothiefroot_response.json(),use_container_width=True)
+        if search_on:
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
+            sf_df=st.dataframe( data=smoothiefroot_response.json(),use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
